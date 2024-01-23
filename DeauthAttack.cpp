9ca15f40dc03bb5f0be22b000c020770a677e619
juwon334 <juwon334@gmail.com>
2024-01-24 00:30:03 +0900

    FirstFinish

--- DeauthAttack.cpp
+++ DeauthAttack.cpp
@@ -3,11 +3,12 @@
 int main(int argc, char* argv[]){
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t* handle = pcap_open_live(argv[1], 0, 0, 0, errbuf);
+
     if (handle == NULL) {
         std::cerr << "network interface '" << argv[1] << "' is down: " << errbuf << std::endl;
         return 1;
     }
-    printf("%d\n",argc);
+
     switch (argc) {
         case 3:
             ApBroadcast(handle, argv[2]);
